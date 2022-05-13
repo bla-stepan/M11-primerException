e89bba1b4c1cf0b1e55ad7b539d68c814b8f4cce
Stepan <bla-stepan@mail.ru>
2022-05-13 10:18:36 +0300

    Пример из модуля 11.4. Программа перекодировщика изменена, добавлено собственное исключение

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,22 @@ import java.io.FileOutputStream;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.UnsupportedCharsetException;
+//создадим свое исключение
 
+class ConvertException extends Exception {
+    ConvertException(String message) {
+        super(message);
+    }
+}
 
 public class Main {
     public static void main(String[] args) {// выкинем throws IOException {
         //ВАРИАНТ ВЕБИНАР через стринг
-        if (copyFileUsingStream1("src/win1251", "windows-1251", "src/utf8", "utf-8")) {
+        try {
+            copyFileUsingStream1("src/utf8", "utf-8", "src/win1251", "windows-1251");
             System.out.println("Перекодировка прошла успешно");
+        } catch (ConvertException e) {
+            System.out.println(e.getMessage());
         }
         //FileNotFoundException перехватываем через surround with Try/catch, предлагаемого IDEA (красная лампочка) или вызывается контекстное меню при нажатии альт энтер на красном объекте
         //ВАРИАНТ МОДУЛЬ через файл
@@ -20,7 +29,8 @@ public class Main {
     }
 
     //ВАРИАНТ ВЕБИНАР через стринг (модернизировали код с использованием принципа try-with-resource) код значительно сокращается
-    private static boolean copyFileUsingStream1(String source, String sourceEnc, String dest, String destEnc) {// throws IOException {
+    //если программа будет выбрасывать только одно исключение, то возвращаемое логическое значение не требуется метод должен быть void все логические ретурны убираются
+    private static void copyFileUsingStream1(String source, String sourceEnc, String dest, String destEnc) throws ConvertException {// throws IOException {
         //Контрол+Альт+Т вызов контекстного меню с обвертками в том числе и Try/catch
         //Charset sEnc = null;//создаем кодировку исходную
         //В скобки после трай засовываем создание переменной потока чтение и переменной потока написание
@@ -34,17 +44,13 @@ public class Main {
             }
             //далее начинаем ловить исключения возникающие при работе программы
         } catch (FileNotFoundException e) {
-            System.out.println("Проблема с файлами: "+ e.getMessage());//теряем информативность т.к. проблемы при создании выходного файла сюда тоже входят
+            throw new ConvertException("Проблема с файлами: " + e.getMessage());//теряем информативность т.к. проблемы при создании выходного файла сюда тоже входят
             //сообщение приходится дополнить e.getMessage()
-            return false;
         } catch (UnsupportedCharsetException e) {
-            System.out.println("Проблема с кодировкой: "+ e.getMessage());
-            return false;
+            throw new ConvertException("Проблема с кодировкой: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Проблема при копировании");
-            return false;
+            throw new ConvertException("Проблема при копировании");
         }
-        return true;
     }
 
     //ВАРИАНТ МОДУЛЬ через файл
